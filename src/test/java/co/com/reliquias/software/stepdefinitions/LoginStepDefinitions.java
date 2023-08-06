bb8b9063ffed06768ccbfefa98680045cbdf643e
jesusTimote <jesus_scorpio_21@hotmail.com>
2023-08-06 18:41:22 -0500

    Automatizacion con selenium ,serenity

---
+++ src/test/java/co/com/reliquias/software/stepdefinitions/LoginStepDefinitions.java
@@ -0,0 +1,23 @@
+package co.com.reliquias.software.stepdefinitions;
+
+import io.cucumber.java.es.Cuando;
+import io.cucumber.java.es.Dado;
+import io.cucumber.java.es.Entonces;
+
+public class LoginStepDefinitions {
+
+    @Dado("que el usuario navega a la página de inicio de sesión")
+    public void navegarPaginaInicioSesion() {
+        //Mi codigo en lenguaje JAva con la logica necesaria para ejecutar mi flujo automatizado
+    }
+
+    @Cuando("ingresa las credenciales de acceso correctas")
+    public void ingresaLasCredencialesDeAccesoCorrectas() {
+    }
+
+    @Entonces("debería ver la página principal")
+    public void deberiaVerPaginaPrincipal(){
+
+    }
+
+}
